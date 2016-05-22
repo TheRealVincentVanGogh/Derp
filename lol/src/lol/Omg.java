e63a81368b37fff49acdfabfca3043eadf5d8dbe
Vincent Ye <vincentye@icloud.com>
2016-05-22 14:35:43 -0700

    Minecraft Hello Message
    Bucket Yay it worked plugin code.

---
+++ lol/src/lol/Omg.java
@@ -0,0 +1,32 @@
+package lol;
+ 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+ 
+public class Omg extends JavaPlugin {
+ 
+    @Override
+    public void onEnable() {
+       
+    }
+   
+    @Override
+    public void onDisable() {
+       
+    }
+   
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String Label, String[] args) {
+    	
+    	if (cmd.getName().equalsIgnoreCase("test") && sender instanceof Player) {
+    		
+    		Player player = (Player) sender;
+    		
+    		player.sendMessage("Yay it worked! Congrats" + player.getName());
+        }
+        return false;
+    }
+   
+}
